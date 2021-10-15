62c8cb50cb020c4b9206dcd7d006cb7bbc5b559f
sogekng <gutsantos3@gmail.com>
2021-10-15 00:14:23 -0300

    Arrumando alguns erros funcionais

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def loginAdm():
             password = senha.readlines()
             if usuarioLogin in usuario and usuarioSenha in password:
                 print('Seja bem-vindo ao Project University - Anhembi Morumbi!')
-                time.sleep(3.5)
+                time.sleep(2.0)
 
                 return False
             else:
@@ -42,14 +42,14 @@ def cadastrarUsuarios(usuarios):
             print("\n" + 10*"_" + f"Cadastro de Usuários" + 10*"_")
 
             while qtdCadastro != contador:
-                nome = input("\nDigite o nome completo do aluno: ").lower()
+                nome = input("\nDigite o nome completo do aluno: ").title()
                 email = input("Digite o e-mail do aluno: ").lower()
 
                 if nome != '' and email != '':
-                    usuarios.append((nome.title(), email))
+                    usuarios.append((nome.strip(), email.strip()))
                     contador += 1
                 else:
-                    print("[Error]Preencha corretamente seus dados")    
+                    print("[Error]Não salvamos espaço em branco amigo")    
                     continue   
             break
 
@@ -93,9 +93,9 @@ def buscarUsuarioEmail(usuarios):
                 print('\nO aluno foi encontrado como:')
                 print(f'Nome: {nome}') 
                 print(f'E-mail: {email}')
-                break
-        else:
-            print("[Error]E-mail não encontrado")
+            else:
+                print("[Error]E-mail não encontrado")
+            break
 
 
 def removerUsuarios(usuarios, usuariosExcluidos):
@@ -107,8 +107,10 @@ def removerUsuarios(usuarios, usuariosExcluidos):
             nome, email = usuario
 
             if email == buscar:
-                print(f'O aluno foi encontrado como > Nome: {nome}, E-mail: {email}')
-                motivoExclusao = input("Qual motivo da exclusão?: ")
+                print(f'\nO aluno foi encontrado como:')
+                print(f'Nome: {nome}') 
+                print(f'E-mail: {email}')
+                motivoExclusao = input("\nQual motivo da exclusão?: ")
                 atualizar = input("\nDeseja remover o cadastro do aluno?[s/n]: ").lower()
                 excluidos = {'nome': nome,'email': email,'motivo': motivoExclusao}
                 if atualizar in ['s', 'sim', 'y', 'yes']:
@@ -116,15 +118,15 @@ def removerUsuarios(usuarios, usuariosExcluidos):
                     usuariosExcluidos.append(excluidos)
                     usuarios.pop(excluir)
                     print(f"\nO cadastro do aluno foi excluído")
-                    break
                 elif atualizar in ['n', 'nao', 'no', 'não']:
                     print("\nVoltando ao menu")
-                    break
                 else:
                     print("\n[Error]Não entendi, pode repetir?")
                     continue
-        else:
-            print(f'[Error]E-mail não encontrado')  
+            else:
+                print(f'[Error]E-mail não encontrado')
+            break
+
 
 def exibirUsuariosExcluidos(usuariosExcluidos):
     if usuariosExcluidos == []:
@@ -132,63 +134,78 @@ def exibirUsuariosExcluidos(usuariosExcluidos):
     else:
         print("\n" + 13*"_" + f"Usuários Excluídos" + 13*"_")
         for usuario in usuariosExcluidos:           
-            print("Nome: {}, E-mail: {}, Motivo: {}".format(usuario['nome'], usuario['email'], usuario['motivo']))
+            print("\nNome: {}\nE-mail: {}\nMotivo: {}".format(usuario['nome'], usuario['email'], usuario['motivo']))
 
 
 def atualizarUsuarios(usuarios):
     if usuarios == []:
         print("[Error]Nenhum aluno cadastrado")
     else:
-        buscar = input("E-mail do aluno a ser atualizado: ")
+        buscar = input("E-mail do aluno a ser atualizado: ").lower().strip()
+
         for i,usuario in enumerate(usuarios):
             nome, email = usuario
 
             if email == buscar:
-                print(f'O aluno foi encontrado como:')
+                print(f'\nO aluno foi encontrado como:')
                 print(f'Nome: {nome}') 
                 print(f'E-mail: {email}')
 
-                novoNome = input("\nDigite o novo nome a ser colocado no seu cadastro: ").lower()
 
-                excluir = usuarios.index(usuario)
-                usuarios.pop(excluir)
+                while True:
+                    novoNome = input("\nDigite o novo nome a ser colocado no seu cadastro: ").title()
 
-                novoUsuario = novoNome.title(), email
+                    if novoNome != "":
+                        excluir = usuarios.index(usuario)
+                        usuarios.pop(excluir)
 
-                usuarios.insert(i, novoUsuario)
-                print(f"\nO nome do aluno foi atualizado, com sucesso.")
+                        novoUsuario = novoNome.strip(), email
+
+                        usuarios.insert(i, novoUsuario)
+                        print(f"\nO nome do aluno foi atualizado, com sucesso.")
+                        break
+                    else:
+                        print("[Error]Não salvamos espaço em branco amigo")
+                        continue
                 break
         else:
             print("[Error]E-mail não encontrado")
+
 
 def atualizarEmail(usuarios):
     if usuarios == []:
         print("[Error]Nenhum aluno cadastrado")
     else:
-        buscar = input("Digite o nome completo do aluno que deseja atualizar o e-mail: ").title()
+        buscar = input("Nome do aluno a ser atualizado: ").title().strip()
+
         for i,usuario in enumerate(usuarios):
             nome, email = usuario
-            if nome == buscar:
-                    print('O aluno foi encontrado como:')
-                    print(f'Nome: {nome}')
-                    print(f'E-mail: {email}')
 
+            if nome == buscar:
+                print(f'\nO aluno foi encontrado como:')
+                print(f'Nome: {nome}')
+                print(f'E-mail: {email}')
+
+                while True:
                     novoEmail = input("\nDigite o novo e-mail a ser colocado no seu cadastro: ").lower()
 
-                    excluir = usuarios.index(usuario)
-                    usuarios.pop(excluir)
+                    if novoEmail != "":
+                        excluir = usuarios.index(usuario)
+                        usuarios.pop(excluir)
 
-                    novoUsuario = nome, novoEmail
+                        novoUsuario = nome, novoEmail.strip()
 
-                    usuarios.insert(i, novoUsuario)
-                    print(f"\nO e-mail do aluno foi atualizado!")
-                    break
-            else:
-                print("[Error]E-mail não encontrado!")
+                        usuarios.insert(i, novoUsuario)
+                        print(f"\nO e-mail do aluno foi atualizado, com sucesso.")
+                        break
+                    else:
+                        print("[Error]Não salvamos espaço em branco amigo")
+                        continue
+                break
+        else:
+            print("[Error]Nome não encontrado")
 
     
-
-
 def back():
     while True:
         back = str(input('\nPRESSIONE "ENTER" PARA VOLTAR AO MENU'))
